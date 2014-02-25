0efdf39019f83f2bba55c0be5c67335304fa6f2d
Fayaaz Ahmed <fayaaz.ahmed@gmail.com>
2014-02-25 18:30:11 +0000

    move import

--- djipster/urls.py
+++ djipster/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-import os
 from django.conf.urls.static import static
 from django.conf import settings
 admin.autodiscover()
+import os
 
 
 urlpatterns = patterns('',
