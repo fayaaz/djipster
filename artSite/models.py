6083e7367084832e0d541f0eb87ef920a26fcf63
Fayaaz Ahmed <fayaaz.ahmed@gmail.com>
2014-04-22 21:10:05 +0100

    Updated models
    [1]Added description to art pieces and [1]fixed image upload

--- artSite/models.py
+++ artSite/models.py
@@ -4,6 +4,8 @@ from markdown import markdown
 from django.core.exceptions import ValidationError
 from django.dispatch import Signal
 from imagefuncs import imgResize, imgRename
+from djipster.settings import BASE_DIR
+import os
 
 def validate_only_one_instance(obj):
     ''' Used for making sure only one entry exists on a model. For example the title of the site '''
@@ -43,10 +45,10 @@ class Gallery(models.Model):
     image_res = models.FileField(default = 'notResized', upload_to = upload_folder, blank = True, null = True, editable = False)
 
     def save(self):
+        super(Art, self).save()
         self.image_res = str(imgRename(str(self.image)))
-        super(Gallery, self).save()
-        
-        resizeTry('media/'+str(self.image))
+        resizeTry(os.path.join(BASE_DIR, 'media', str(self.image)))
+        super(Art, self).save()
     
     def __unicode__(self):
         return self.title
@@ -63,12 +65,12 @@ class Project(models.Model):
     
     
     def save(self):
+        ''' Resize uploaded image by saving the model first, resizing and then saving again '''
+        super(Art, self).save()
         self.image_res = str(imgRename(str(self.image)))
-        super(Project, self).save()
+        resizeTry(os.path.join(BASE_DIR, 'media', str(self.image)))
+        super(Art, self).save()
         
-        resizeTry('media/'+str(self.image))
-            
-    
     def __unicode__(self):
         return self.title
     
@@ -82,13 +84,16 @@ class Art(models.Model):
     gallery = models.ForeignKey('Gallery',  null = True, blank = True)
     image = models.FileField(upload_to = upload_folder)
     image_res = models.FileField(default = 'notResized', upload_to = upload_folder, blank = True, null = True, editable = False)
+    description_markdown = models.TextField('Description', help_text='Write in Markdown! <a href=\'https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet/\'  target=\'_blank\'>Help</a>', blank=True, null=True)
+    description = models.TextField('Description HTML', help_text='HTML from markdown : Do not edit me!', blank=True, null = True)
     
     def save(self):
+        ''' Resize uploaded image by saving the model first, resizing and then saving again '''
+        super(Art, self).save()
         self.image_res = str(imgRename(str(self.image)))
+        resizeTry(os.path.join(BASE_DIR, 'media', str(self.image)))
         super(Art, self).save()
         
-        resizeTry('media/'+str(self.image))
-
     def __unicode__(self):
         return self.title
     
@@ -96,7 +101,7 @@ class About(models.Model):
     '''Each section in About. To be written in Markdown (body_markdown field) to allow editing in CMS.'''
     title = models.CharField(max_length=50)
     body_markdown = models.TextField('Entry Body', help_text='Write in Markdown! <a href=\'https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet/\'  target=\'_blank\'>Help</a>', blank=True)
-    body = models.TextField('Entry HTML', help_text='HTML from markdown', blank=True)
+    body = models.TextField('Entry HTML', help_text='HTML from markdown : Do not edit me!', blank=True)
     image = models.FileField(upload_to = 'upload/about', blank=True)
     
     def __unicode__(self):
